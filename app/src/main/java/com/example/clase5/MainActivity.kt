577fe7800efddb1b15aa55de41e77ee298bd72fd
waltermedinacode <medinawalter3@gmail.com>
2026-03-16 20:20:35 -0500

    update code

--- app/src/main/java/com/example/clase5/MainActivity.kt
+++ app/src/main/java/com/example/clase5/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     private fun setupToolbar() {
         val toolbar = binding.contentToolbar.toolbar
         setSupportActionBar(toolbar)
-        //setupDrawer(toolbar)
+        setupDrawer(toolbar)
         toolbar.title = "Activity Uno"
         toolbar.setOnMenuItemClickListener { item ->
             when (item.itemId) {
@@ -52,39 +52,39 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-//    private fun setupDrawer(toolbar: Toolbar) {
-//
-//        val drawerLayout = binding.drawerLayout
-//        val navView = binding.navView
-//        navView.itemIconTintList = null //para que aplique el color original de los iconos
-//        // para implementar un botón de navegación en el toolbar
-//        val toggle = ActionBarDrawerToggle(
-//            this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer
-//        )
-//        // Aplica el color a tu Drawable
-//        toggle.drawerArrowDrawable.color = ContextCompat.getColor(this,R.color.white)
-//        drawerLayout.addDrawerListener(toggle)
-//        toggle.syncState()//permite mostrar el icono amburguesa
-//
-//        // Configurar el listener para los elementos del Navigation Drawer
-//        navView.setNavigationItemSelectedListener { menuItem ->
-//            when (menuItem.itemId) {
-//                R.id.nav_item1 -> {
-//                    startActivity(Intent(this, MainActivity2::class.java))
-//                    drawerLayout.closeDrawer(navView)//para cerra el layout del drawer
-//                    true
-//                }
-//
-//                R.id.nav_item2 -> {
-//                    Toast.makeText(this, "Opcion 2", Toast.LENGTH_LONG).show()
-//                    drawerLayout.closeDrawer(navView)
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
-//
-//    }
+    private fun setupDrawer(toolbar: Toolbar) {
+
+        val drawerLayout = binding.drawerLayout
+        val navView = binding.navView
+        navView.itemIconTintList = null //para que aplique el color original de los iconos
+        // para implementar un botón de navegación en el toolbar
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer
+        )
+        // Aplica el color a tu Drawable
+        toggle.drawerArrowDrawable.color = ContextCompat.getColor(this,R.color.white)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()//permite mostrar el icono amburguesa
+
+        // Configurar el listener para los elementos del Navigation Drawer
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_item1 -> {
+                    startActivity(Intent(this, MainActivity2::class.java))
+                    drawerLayout.closeDrawer(navView)//para cerra el layout del drawer
+                    true
+                }
+
+                R.id.nav_item2 -> {
+                    Toast.makeText(this, "Opcion 2", Toast.LENGTH_LONG).show()
+                    drawerLayout.closeDrawer(navView)
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
 
 
 }
